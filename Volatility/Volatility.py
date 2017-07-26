9619102d53f1dfeb1066c577439e4ad24cf889de
CharlMeyers <meyers.charl@gmail.com>
2017-07-26 21:45:56 +0200

    Add basic UI layout

--- Volatility/Volatility.py
+++ Volatility/Volatility.py
@@ -1,7 +1,15 @@
-from org.sleuthkit.autopsy.ingest import IngestModuleFactoryAdaptor
+from java.awt import GridBagLayout
+from java.awt import GridBagConstraints
+from javax.swing import JPanel
+from javax.swing import JLabel
+from javax.swing import JTextField
+from javax.swing import JButton
+
+from org.sleuthkit.autopsy.ingest import IngestModuleFactoryAdapter
 from org.sleuthkit.autopsy.ingest import IngestModuleIngestJobSettingsPanel
 
-class IngestModuleFactory(IngestModuleFactoryAdaptor):
+
+class IngestModuleFactory(IngestModuleFactoryAdapter):
     def __init__(self):
         self.settings = None
         self.moduleName = "Volatility Processor"
@@ -15,16 +23,16 @@ class IngestModuleFactory(IngestModuleFactoryAdaptor):
     def getModuleVersionNumber(self):
         return "1.0"
 
-    def getDefaultIngestJobSettings(self):
-        return None
+    # def getDefaultIngestJobSettings(self):
+    #     return None
 
     def hasIngestJobSettingsPanel(self):
         return True
 
     def getIngestJobSettingsPanel(self, settings):
-        self.settings settings
+        self.settings = settings
 
-        return SettingsPanel(self.settings)
+        return SettingsUIPanel(self.settings)
 
     def isDataSourceIngestModule(self):
         True
@@ -32,6 +40,56 @@ class IngestModuleFactory(IngestModuleFactoryAdaptor):
     def createDataSourceIngestModule(self, ingestOptions):
         return None
 
-class SettingsPanel(IngestModuleIngestJobSettingsPanel):
-    def __init__(self,settings):
+
+class SettingsUIPanel(IngestModuleIngestJobSettingsPanel):
+    def __init__(self, settings):
         self.local_settings = settings
+        self.initLayout()
+
+    def initLayout(self):
+        self.mainPanel = JPanel()
+
+        self.gridBagPanel = GridBagLayout()
+        self.gridBagConstraints = GridBagConstraints()
+        self.mainPanel.setLayout(self.gridBagConstraints)
+
+        # Volatility Executable Path
+        self.dirLabel = JLabel("Volatility Executable Directory")
+        self.dirLabel.setEnabled(True)
+        self.gridBagConstraints.gridx = 2
+        self.gridBagConstraints.gridy = 1
+        self.gridBagConstraints.gridwidth = 1
+        self.gridBagConstraints.gridheight = 1
+        self.gridBagConstraints.fill = GridBagConstraints.BOTH
+        self.gridBagConstraints.weightx = 1
+        self.gridBagConstraints.weighty = 0
+        self.gridBagConstraints.anchor = GridBagConstraints.NORTH
+        self.gridBagPanel.setConstraints(self.dirLabel, self.gridBagConstraints)
+        self.mainPanel.add(self.dirLabel)
+
+        self.volatilityDirTextField = JTextField(10)
+        self.volatilityDirTextField.setEnabled(True)
+        self.gridBagConstraints.gridx = 2
+        self.gridBagConstraints.gridy = 3
+        self.gridBagConstraints.gridwidth = 1
+        self.gridBagConstraints.gridheight = 1
+        self.gridBagConstraints.fill = GridBagConstraints.BOTH
+        self.gridBagConstraints.weightx = 1
+        self.gridBagConstraints.weighty = 0
+        self.gridBagConstraints.anchor = GridBagConstraints.NORTH
+        self.gridBagPanel.setConstraints(self.volatilityDirTextField, self.gridBagConstraints)
+        self.mainPanel.add(self.volatilityDirTextField)
+
+        self.FindVolatilityPathButton = JButton("Find Dir", actionPerformed=self.Find_Dir)
+        self.FindVolatilityPathButton.setEnabled(True)
+        self.rbgmainPanel.add(self.FindVolatilityPathButton)
+        self.gridBagConstraints.gridx = 6
+        self.gridBagConstraints.gridy = 3
+        self.gridBagConstraints.gridwidth = 1
+        self.gridBagConstraints.gridheight = 1
+        self.gridBagConstraints.fill = GridBagConstraints.BOTH
+        self.gridBagConstraints.weightx = 1
+        self.gridBagConstraints.weighty = 0
+        self.gridBagConstraints.anchor = GridBagConstraints.NORTH
+        self.gridBagPanel.setConstraints(self.FindVolatilityPathButton, self.gridBagConstraints)
+        self.mainPanel.add(self.FindVolatilityPathButton)
